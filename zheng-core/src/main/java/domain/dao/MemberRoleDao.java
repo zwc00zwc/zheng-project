fd96b760199c9b6b0ca4cbb0033dd7fed047e129
zhengwenchao <zhengwenchao17@163.com>
2016-08-24 14:02:56 +0800

    sql

--- zheng-core/src/main/java/domain/dao/MemberRoleDao.java
+++ zheng-core/src/main/java/domain/dao/MemberRoleDao.java
@@ -1,7 +1,12 @@
 package domain.dao;
 
+import domain.model.system.MemberRole;
+
+import java.util.List;
+
 /**
  * Created by XR on 2016/8/23.
  */
 public interface MemberRoleDao {
+    List<MemberRole> queryRolesByMemberid(Long memberid);
 }
--- zheng-core/src/main/java/domain/dao/PermDao.java
+++ zheng-core/src/main/java/domain/dao/PermDao.java
@@ -1,7 +1,14 @@
 package domain.dao;
 
+import domain.model.system.Perm;
+
 /**
  * Created by XR on 2016/8/22.
  */
 public interface PermDao {
+    Perm queryById(Long id);
+
+    int insertPerm(Perm perm);
+
+    int updateById(Perm perm);
 }
