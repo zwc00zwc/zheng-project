49a41104fca39f3aa82bc71e35bcedd5b27df8f3
zhengwenchao <zhengwenchao17@163.com>
2016-08-30 10:55:50 +0800

    mongodb

--- zheng-common/src/main/java/mongodb/MongodbManager.java
+++ zheng-common/src/main/java/mongodb/MongodbManager.java
@@ -1,13 +1,38 @@
 package mongodb;
 
-import com.mongodb.MongoClient;
+import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/8/21.
  */
 public class MongodbManager {
-    public static MongoClient getMongoClient(String host, int port){
+    public static MongoDatabase getMongoClient(String host, int port,String database){
         MongoClient client=new MongoClient(host,port);
-        return client;
+
+        return client.getDatabase(database);
+    }
+
+    public static MongoDatabase getMongoClient(String host, int port, String username, String database, String password){
+        List<ServerAddress> addresses=new ArrayList<ServerAddress>();
+        ServerAddress serverAddress=new ServerAddress(host,port);
+        addresses.add(serverAddress);
+        List<MongoCredential> credentials=new ArrayList<MongoCredential>();
+        MongoCredential mongoCredential= MongoCredential.createScramSha1Credential(username,database,password.toCharArray());
+        credentials.add(mongoCredential);
+        MongoClient mongoClient=new MongoClient(addresses,credentials);
+        return mongoClient.getDatabase(database);
+    }
+
+    /**
+     * 插入
+     * @param collection
+     * @param basicDBObject  BasicDBObject实现DBObject接口
+     */
+    public static void insert(DBCollection collection,BasicDBObject basicDBObject){
+        collection.insert(basicDBObject);
     }
 }
