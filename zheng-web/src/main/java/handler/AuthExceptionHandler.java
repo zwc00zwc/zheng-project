fa0e901279a8a39b8244d16fd353877ae26cd203
zwc00zwc <1484659036@qq.com>
2016-08-30 21:58:02 +0800

    功能

--- zheng-web/src/main/java/handler/AuthExceptionHandler.java
+++ zheng-web/src/main/java/handler/AuthExceptionHandler.java
@@ -1,7 +1,7 @@
 package handler;
 
 import exception.AuthException;
-import log.LogUtility;
+import exception.LoginException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,12 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthExceptionHandler implements HandlerExceptionResolver {
     public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
         //LogUtility.insertLog(e.getClass().getName(),e.getMessage());
+        String requestheader= httpServletRequest.getHeader("X-Requested-With");
         if (e instanceof AuthException){
-            String requestheader= httpServletRequest.getHeader("X-Requested-With");
             if (requestheader!=null){
                 return new ModelAndView("redirect:/nologin");
             }
             return new ModelAndView("redirect:/login");
+        }
+        if (e instanceof LoginException){
+            if (requestheader!=null){
+                return new ModelAndView("redirect:/noauthorized/ajax");
+            }
+            return new ModelAndView("redirect:/noauthorized");
         }
         return new ModelAndView("redirect:/404");
     }
