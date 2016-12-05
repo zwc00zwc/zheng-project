e90f1fd2df42a263365775da2ebea734cab644e9
zwc00zwc <z09024931>
2016-12-05 15:08:32 +0800

    spring boot

---
+++ zheng-boot/src/main/java/Application.java
@@ -0,0 +1,27 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+/**
+ * Created by XR on 2016/12/5.
+ */
+@SpringBootApplication
+public class Application {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        for (int i=0;i<10;i++){
+            try {
+                Thread.sleep(1000);
+                System.out.print("现在时间"+new Date().toString());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
