8ab78b7b41eb584d3f7585c9366c4e8d182aca38
zhengwenchao <zheng.wenchao@yrw.com>
2017-11-13 11:30:04 +0800

    mock test

---
+++ zheng-web/src/test/java/testController/HomeControllerTest.java
@@ -0,0 +1,41 @@
+package testController;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+/**
+ * Created by alan.zheng on 2017/11/13.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath*:applicationContext.xml")
+@WebAppConfiguration
+public class HomeControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void demoTest() throws  Exception{
+        MvcResult result = mockMvc.perform(get("/welcome")).andReturn();
+        System.out.println("-----------------------------");
+        //System.out.println(byMobile);
+    }
+}
