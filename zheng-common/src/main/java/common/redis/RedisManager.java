fa31dff88b3eb0c832f8f128b847a3c3cfc2245a
zhengwenchao <zheng.wenchao@yrw.com>
2018-04-16 10:40:27 +0800

    合并

--- zheng-common/src/main/java/common/redis/RedisManager.java
+++ zheng-common/src/main/java/common/redis/RedisManager.java
@@ -2,10 +2,13 @@ package common.redis;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.lang3.SerializationUtils;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.exceptions.JedisConnectionException;
+
+import java.io.Serializable;
 
 /**
  * Created by Administrator on 2016/8/21.
@@ -21,7 +24,7 @@ public class RedisManager {
         jedisPoolConfig.setMaxWaitMillis(-1);
         //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
         jedisPoolConfig.setTestOnBorrow(true);
-        jedisPool=new JedisPool(jedisPoolConfig,"127.0.0.1",6379);
+        jedisPool=new JedisPool(jedisPoolConfig,"192.168.48.129",6379);
         return jedisPool;
     }
 
@@ -136,5 +139,94 @@ public class RedisManager {
                 returnResource(jedis);
         }
         return result;
+    }
+
+    public static Object getObject(String key){
+        Jedis jedis = null;
+        Object value = null;
+        boolean borrowOrOprSuccess = true;
+        try {
+            jedis = getClient();
+//            jedis.select(PropertiesUtil.getObjectCacheDBIndex());
+            value = SerializationUtils.deserialize(jedis.get(SerializationUtils
+                    .serialize(key.hashCode())));
+        } catch (JedisConnectionException e) {
+//            logger.error("redis,获取 key={}的数据" , key, e);
+            borrowOrOprSuccess = false;
+            if (jedis != null)
+                jedisPool.returnBrokenResource(jedis);
+        } finally {
+            if (borrowOrOprSuccess)
+                jedisPool.returnResource(jedis);
+        }
+//        if (logger.isDebugEnabled())
+//            logger.debug("getObject:" + key.hashCode() + "=" + value);
+        return value;
+    }
+
+    public static boolean isExitByObjectKey(Object key) {
+        Jedis jedis = null;
+        boolean value = false;
+        boolean borrowOrOprSuccess = true;
+        try {
+            jedis = getClient();
+//            jedis.select(PropertiesUtil.getObjectCacheDBIndex());
+            value = jedis.exists(SerializationUtils.serialize(key.hashCode()));
+        } catch (JedisConnectionException e) {
+            borrowOrOprSuccess = false;
+            if (jedis != null)
+                jedisPool.returnBrokenResource(jedis);
+        } finally {
+            if (borrowOrOprSuccess)
+                jedisPool.returnResource(jedis);
+        }
+//        if (logger.isDebugEnabled())
+//            logger.debug("getObject:" + key.hashCode() + "=" + value);
+        return value;
+    }
+
+    public static void putObject(Object key, Object value) {
+        Jedis jedis = null;
+        boolean borrowOrOprSuccess = true;
+        Serializable serializeble = null;
+        if (value instanceof java.io.Serializable) {
+            serializeble = (Serializable) value;
+        } else {
+//            logger.error("对象未序列化");
+            return;
+        }
+        try {
+            jedis = getClient();
+            jedisPool = RedisManager.getJedisPool();
+            jedis.set(SerializationUtils.serialize(key.hashCode()), SerializationUtils.serialize(serializeble));
+        } catch (JedisConnectionException e) {
+            borrowOrOprSuccess = false;
+            if (jedis != null)
+                jedisPool.returnBrokenResource(jedis);
+        } finally {
+            if (borrowOrOprSuccess)
+                jedisPool.returnResource(jedis);
+        }
+
+    }
+
+    public static Object removeObject(Object key) {
+        Jedis jedis = null;
+        Object value = null;
+        boolean borrowOrOprSuccess = true;
+        try {
+            jedis = getClient();
+            value = jedis.expire(SerializationUtils.serialize(key.hashCode()), 0);
+        } catch (JedisConnectionException e) {
+            borrowOrOprSuccess = false;
+            if (jedis != null)
+                jedisPool.returnBrokenResource(jedis);
+        } finally {
+            if (borrowOrOprSuccess)
+                jedisPool.returnResource(jedis);
+        }
+//        if (logger.isDebugEnabled())
+//            logger.debug("getObject:" + key.hashCode() + "=" + value);
+        return value;
     }
 }
